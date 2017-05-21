9808a34791619fe3f3afbacc539c876352c5b75a
Melissa Hernandez <melissahernandez@cpe-172-100-1-139.twcny.res.rr.com>
2017-05-21 18:28:47 -0500

    updated http to https

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,7 +8,7 @@ function displayGifs() {
 	$("#tv-gifs").empty();	
 
 	var show = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
 		url: queryURL,
